29b46d0c3f08924a582713d8174376a381bce4c9
Erik Newton <eriknewt@gmail.com>
2020-04-02 18:00:25 -0500

    started transaction.js

---
+++ src/main/resources/static/scripts/transaction.js
@@ -0,0 +1,11 @@
+/*
+TODO: Add functionality to search
+    <div id='searchArea'>
+            <h4>Product Lookup Code</h4>
+            <input id='searchFor' type='text' size='36'><br><br>
+            <button id='searchBtn' class='btn'>Search</button><br><br>
+
+            <textarea id="returned" rows='6' cols='64' style="visibility: visible">returned search results...</textarea><br><br>
+            <button id="add" class='btn' style="visibility: visible">Add</button><br><br>
+        </div> 
+*/
