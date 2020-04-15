ce1feded5ea5ee2fa30b42fd57be355ff1e33ac2
Mitch <emnorris@uark.edu>
2020-04-14 21:09:01 -0500

    added broken function to add products to cart, removes item from search area, clear search area when searching again

--- src/main/resources/static/scripts/transaction.js
+++ src/main/resources/static/scripts/transaction.js
@@ -5,9 +5,14 @@ document.addEventListener("DOMContentLoaded", function(event) {
 	if(!isEmpty){
 		document.getElementById("searchBtn").addEventListener("click", searchActionClick);
 	}
+	const listItemElements = document.getElementById("createProductList").querySelectorAll("li");
+	for(let i = 0; i < listItemElements.length; i++) {
+		listItemElements[i].addEventListener("click", onListItemClicked);
+	}
 });
 
 function validateForm() {
+	removeProductList();
     var lookupCode = document.forms["Search"]["lookupCode"].value;
     	
 	if (lookupCode == "") {
@@ -16,8 +21,25 @@ function validateForm() {
 		return false;
 	}
 	document.getElementById("msg").innerHTML = "";
+
 	isEmpty = false;
 	return true;
+}
+
+function getCLickedListItemElement(target) {
+	let clickedElement = target;
+
+	while(clickedElement.tagName !== "LI") {
+		clickedElement = clickedElement.parentElement;
+	}
+
+	return clickedElement;
+}
+
+function onListItemClicked(event) {
+	const unorderedListElement = document.getElementById("createProductList");
+	unorderedListElement.removeChild(getCLickedListItemElement(event.target));
+
 }
 
 function searchActionClick(event) {
@@ -26,11 +48,10 @@ function searchActionClick(event) {
 	const searchActionUrl = ("/api/transaction/?lookupCode=" + getLookupCode());
 
 	if (getLookupCode() != null){
+
 		var searchResults = ajaxGet(searchActionUrl, (callbackResponse) => {
 			searchActionElement.disabled = false;
-
 			if (isSuccessResponse(callbackResponse)) {
-
 				if (!callbackResponse.data.isEmpty) { 
 					if (callbackResponse.data.length == 0){
 						document.getElementById("msg").innerHTML = "Result not found";
@@ -39,24 +60,45 @@ function searchActionClick(event) {
 						createProductList(callbackResponse.data[i]);
 						console.log(callbackResponse.data[i]);
 					}							
-							
-					
 				}
 			}
 		});
 	}
 };
 
+function addProductToCart(target) {
+
+	let product = getCLickedListItemElement(target);
+	const tableElement = document.getElementById("cart");
+	const trElement = document.createElement("tr");
+	const cartDisplayElement = document.createElement("span");
+	cartDisplayElement.innerHTML = product.innerHTML;
+	cartDisplayElement.classList.add("cart");
+	trElement.appendChild(cartDisplayElement);
+	trElement.appendChild(document.createElement("br"));
+	tableElement.appendChild(trElement);
+}
+
+function removeProductList() {
+	var list = document.getElementById("createProductList");
+
+	let i = 0;
+	while(list.hasChildNodes) {
+		list.removeChild(list.childNodes[i]);
+		i++;
+	}
+}
 
 function createProductList(returnLookupCode) {
 	
 	const ulElement = document.getElementById("createProductList");
 	const nextEntryId = (ulElement.childElementCount + 1).toString();
 	const liElement = document.createElement("li");
+	liElement.addEventListener("click", onListItemClicked);
 	const lookupCodeDisplayElement = document.createElement("Span");
 
 	if (returnLookupCode != "") {
-		lookupCodeDisplayElement.innerHTML = ("Searched Results :" + returnLookupCode);
+		lookupCodeDisplayElement.innerHTML = returnLookupCode;
 		lookupCodeDisplayElement.classList.add("productLookupCodeDisplay");
 		liElement.appendChild(lookupCodeDisplayElement);
 		liElement.appendChild(document.createElement("br"));
